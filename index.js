ac7bb1fac5b039bf1478a032ed0f2c8773de9ba7
AK-Developers <aryankhatri9811@gmail.com>
2023-12-24 12:27:24 +0530

    JavaScript Practice

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
 const time = document.querySelector("#timeToDisplay");
 const startBtn = document.querySelector("#startBtn");
-const pauseBtn = document.querySelector("#pauseBtn"); // Assuming this button exists
+const pauseBtn = document.querySelector("#pauseBtn");
 const resetBtn = document.querySelector("#resetBtn");
 
 let startTime = 0;
 let elapsedTime = 0;
 let intervalId;
+let paused = true; // Initialize paused state
 
 startBtn.addEventListener("click", () => {
-  if (intervalId === undefined) { // Check if timer is already running
-    startTime = Date.now();
-    intervalId = setInterval(displayTime, 1000); // Start the interval
+  if (paused) { // Check for paused state and existing interval
+    paused = false;
+    startTime = Date.now()-elapsedTime;
+    intervalId = setInterval(displayTime, 1000);
+  }
+})
+
+pauseBtn.addEventListener("click", () => {
+  if (!paused) { 
+    paused = true;
+    elapsedTime = Date.now() - startTime;
+    clearInterval(intervalId);
   }
 })
 
 resetBtn.addEventListener("click", () => {
   clearInterval(intervalId); // Stop the interval before resetting
-  intervalId = undefined;
+  startTime = 0; // Reset startTime as well
   elapsedTime = 0;
-  time.textContent = "00:00:00"; 
+  paused = true; // Reset paused state
+  time.textContent = "00:00:00";
 })
 
 function displayTime() {
-  elapsedTime = Date.now() - startTime;
-  const hrs = Math.floor(elapsedTime / (60 * 60 * 1000) % 60);
-  const min = Math.floor(elapsedTime / (60 * 1000) % 60);
-  const sec = Math.floor(elapsedTime / 1000 % 60);
+  if (!paused) { // Only update time if not paused
+    elapsedTime = Date.now() - startTime;
+    const hrs = Math.floor(elapsedTime / (60 * 60 * 1000) % 60);
+    const min = Math.floor(elapsedTime / (60 * 1000) % 60);
+    const sec = Math.floor(elapsedTime % 60000 / 1000); // More efficient modulo calculation
 
-  const formattedTime = `${head(hrs)}:${head(min)}:${head(sec)}`;
-  time.textContent = formattedTime;
-}
-
-function head(unit) {
-  return unit.toString().padStart(2, '0'); // More concise way to add leading zeros
+    const formattedTime = `${head(hrs)}:${head(min)}:${head(sec)}`;
+    time.textContent = formattedTime;
+    }
+    function head(unit) {
+        return unit.toString().padStart(2, '0');
+      }
 }
